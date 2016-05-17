1563c758a329daeaff2451f2e233a819654a433f
Stefan Malewski <alviprofluvium@gmail.com>
2016-05-16 23:10:35 -0400

    Remove vim directory and cleaned xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,22 +1,34 @@
 import XMonad
-import XMonad.Layout.ToggleLayouts
+import XMonad.Hooks.DynamicLog
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Magnifier
 import XMonad.Layout.LimitWindows
 import XMonad.Layout.FixedColumn
 
+--Workspaces
 myWorkspaces = ["1:web","2:code"] ++ map show [3..9]
-myLayout = toggleLayouts Full perWS 
-    where 
-        perWS = onWorkspace "2:code" codeLayout $ standardLayouts
-        codeLayout = limitWindows 3 $ magnifiercz' 1.4 $ FixedColumn 1 1 84 10
-        standardLayouts = Full ||| Tall 1 (3/100) (1/2) ||| codeLayout 
-        
 
-main = xmonad defaultConfig
-    { terminal    = "urxvt"
+--Layouts
+myLayout = onWorkspace "2:code" codeLayout $ standardLayouts
+    where 
+        codeLayout = limitWindows 3 $ magnifiercz' 1.4 $ FixedColumn 1 1 84 10
+        standardLayouts = Full ||| tiled ||| Mirror tiled ||| codeLayout 
+        tiled = Tall 1 (3/100) (1/2)
+
+--Barra
+myBar = "xmobar"
+
+--Terminal
+myTerminal = "urxvt"
+        
+--Configuration
+myConfig = defaultConfig
+    { terminal    = myTerminal
     , modMask     = mod4Mask
     , workspaces  = myWorkspaces
     , layoutHook  = myLayout
     }
+
+--Main function
+main = xmonad =<< xmobar myConfig
 
